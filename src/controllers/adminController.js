13ad03a97ea8642e832c584e431005f91648933d
Mohamadi98 <mohamadinaena23@gmail.com>
2023-08-10 19:59:38 +0300

    creating admin model & implementing adding an admin to the database functionality

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const admin_agent = require('../models/adminModel')
 
 dotenv.config();
 
 const adminRouter = express.Router();
 
-const index = (req, res) => {
-     res.send('admin route running')
+const create = async (req, res) => {
+     const { username, email, password, role, active, profile_image } = req.body
+     console.log(email);
+     const data = {
+          "username": username,
+          "email": email,
+          "password": password, 
+          "role": role,
+          "active": active,
+          "profile_image": profile_image
+     }
+     const new_admin = await admin_agent.create(data);
+     res.json(new_admin);
 }
 
-adminRouter.get('/admin', index);
+adminRouter.post('/admin', create);
+
 module.exports = adminRouter
---
+++ src/models/adminModel.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+const db_client = require('../db_config'); 
+
+const admin_agent = db_client.define('admin', {
+    id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  profile_image: {
+    type: DataTypes.STRING
+  },
+  active: {
+    type: DataTypes.BOOLEAN
+  }
+}, {
+    timestamps: false,
+    tableName: 'admin'
+});
+
+module.exports = admin_agent;
