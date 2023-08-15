e96ab8fc56b683b91e60d107bb525b8ad7b284d4
Mohamadi98 <mohamadinaena23@gmail.com>
2023-08-15 19:34:46 +0300

    implemented an admin login endpoint with a generated jwt

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const adminServices = require('../services/adminServices')
 const adminMiddlewares = require('../middlewares/check_admin_creds_exist')
 const hash_functions = require('../services/hashingServices')
+const tokenServices = require('../services/tokenServices')
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ const add_admin = async (req, res) => {
      const data = {
           username: username,
           email: email,
-          password: hashed_password, 
+          password: hashed_password,
           role: 'admin',
           active: active,
           profile_image: profile_image
@@ -41,7 +42,42 @@ const get_all_admins = async (req, res) => {
      }
 }
 
+const admin_login = async (req, res) => {
+     const { email, password } = req.body;
+     const result = await adminServices.fetch_one_with_email(email);
+     if (result) {
+          const admin_password = result['password'];
+          const verify_password = await hash_functions.verify_hash(password, admin_password);
+          if (verify_password) {
+               const token = await tokenServices.generate_token(result['id'], email);
+               if (result['role'] === 'superadmin') {
+                    res.status(200).json({
+                         'message': 'superadmin user',
+                         'token': token
+                    });
+               }
+               else{
+                    res.status(200).json({
+                         'message': 'admin user',
+                         'token': token
+                    });
+               }
+          }
+          else{
+               res.status(400).json({
+                    'message': 'invalid email or password'
+               });
+          }
+     }
+     else{
+          res.status(400).json({
+               'message': 'invalid email'
+          })
+     }
+}
+
 adminRouter.post('/add-admin', adminMiddlewares.check_exist, add_admin);
 adminRouter.get('/get-all-admins', get_all_admins)
+adminRouter.post('/admin-login', admin_login)
 
 module.exports = adminRouter
