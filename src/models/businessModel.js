8184e17d82c0290fa69e7186a6e81482baca06ae
Mohamadi98 <mohamadinaena23@gmail.com>
2023-08-22 14:23:51 +0300

    implemented business model

---
+++ src/models/businessModel.js
@@ -0,0 +1,57 @@
+const { DataTypes } = require('sequelize');
+const connect_db = require('../db_config'); 
+
+const db_client = connect_db();
+const business_agent = db_client.define('business', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        },
+    name: {
+        type: DataTypes.STRING
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+    phone_no: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+    category: {
+        type: DataTypes.STRING
+    },
+    state: {
+        type: DataTypes.STRING
+    },
+    website: {
+        type: DataTypes.STRING
+    },
+    street: {
+        type: DataTypes.STRING
+    },
+    city_zip: {
+        type: DataTypes.STRING
+    },
+    city1: {
+        type: DataTypes.STRING
+    },
+    city2: {
+        type: DataTypes.STRING
+    },
+    tags: {
+        type: DataTypes.ARRAY(DataTypes.STRING)
+    },
+    logo: {
+        type: DataTypes.STRING
+    },
+    likes: {
+        type: DataTypes.INTEGER
+    }
+}, {
+    timestamps: false,
+    tableName: 'business'
+});
+
+module.exports = business_agent;
