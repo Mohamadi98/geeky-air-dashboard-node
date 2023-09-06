9e59070f2b4709a0a45db3af3d8ab7efe53b549c
Mohamadi98 <mohamadinaena23@gmail.com>
2023-09-06 17:11:34 +0300

    migrating from firebase store to aws s3 store

--- src/services/awsS3Services.js
+++ src/services/awsS3Services.js
@@ -10,13 +10,16 @@ AWS.config.update({
 
 const upload_admin_image_to_s3 = async (base64Image, adminName) => {
     try {
+      if (base64Image.length < 700) {
+        return base64Image
+      }
       const split_on_semicolon = base64Image.split(';')[0];
       const image_extension = split_on_semicolon.split('/')[1];
       const modified_base64 = base64Image.split(',')[1];
       const s3 = new AWS.S3();
       const params = {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `admin-${adminName}-image`,
+      Key: `admin-${adminName}-image.${image_extension}`,
       Body: Buffer.from(modified_base64, 'base64'),
       ContentType: `image/${image_extension}`,
       ACL: 'public-read'
@@ -32,13 +35,16 @@ const upload_admin_image_to_s3 = async (base64Image, adminName) => {
 
 const upload_logo_to_s3 = async (base64Image, businessName) => {
     try {
+      if (base64Image.length < 700) {
+        return base64Image;
+    }
       const split_on_semicolon = base64Image.split(';')[0];
       const image_extension = split_on_semicolon.split('/')[1];
       const modified_base64 = base64Image.split(',')[1];
       const s3 = new AWS.S3();
       const params = {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `${businessName}-logo`,
+      Key: `${businessName}-logo.${image_extension}`,
       Body: Buffer.from(modified_base64, 'base64'),
       ContentType: `image/${image_extension}`,
       ACL: 'public-read'
@@ -56,7 +62,7 @@ const upload_business_image_to_s3 = async (businessName, images_arr) => {
   try {
     let images_url_list = [];
     for (const image of images_arr) {
-        if (image.length < 500) {
+        if (image.length < 700) {
             images_url_list.push(image);
             continue;
         }
@@ -67,7 +73,7 @@ const upload_business_image_to_s3 = async (businessName, images_arr) => {
         const s3 = new AWS.S3();
         const params = {
         Bucket: process.env.AWS_BUCKET_NAME,
-        Key: `${businessName}-image-${image_id}`,
+        Key: `${businessName}-image-${image_id}.${image_extension}`,
         Body: Buffer.from(modified_base64, 'base64'),
         ContentType: `image/${image_extension}`,
         ACL: 'public-read'
@@ -85,13 +91,17 @@ const upload_business_image_to_s3 = async (businessName, images_arr) => {
 
 const upload_video_to_s3 = async (base64Video, businessName) => {
   try {
+    if (base64Video.length < 700) {
+      return base64Video;
+  }
     const split_on_semicolon = base64Video.split(';')[0];
     const video_extension = split_on_semicolon.split('/')[1];
+    console.log(video_extension);
     const modified_base64 = base64Video.split(',')[1];
     const s3 = new AWS.S3();
     const params = {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `${businessName}-video`,
+      Key: `${businessName}-video.${video_extension}`,
       Body: Buffer.from(modified_base64, 'base64'),
       ContentType: `video/${video_extension}`,
       ACL: 'public-read',
