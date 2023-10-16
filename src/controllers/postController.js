fced20bc301256d12dc8f6f75bc92e905c526b5c
Mohamadi98 <mohamadinaena23@gmail.com>
2023-10-16 15:03:58 +0300

    removing u_start_date from recurring month creation

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -157,7 +157,6 @@ const add_post = async (req, res) => {
                 time: request_data['time'],
                 recurring_every: request_data['recurring_every'],
                 recurring_for: request_data['recurring_for'],
-                u_start_date: request_data['u_start_date'],
                 u_end_date: request_data['u_end_date'],
                 u_selected_days: request_data['u_selected_days'],
                 expire_at: endingDate,
